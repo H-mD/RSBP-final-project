91a34fc4c0e4c0d6f11d3b134d85388fd72d4fb7
H-mD <syahrizalpmg@gmail.com>
2023-11-26 10:13:23 +0700

    feat: integrating predict to flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from CGViewBuilder import CGViewBuilder
+from predict import Predict
 import os
 
 app = Flask(__name__)
@@ -7,12 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/diagram-form')
 def diagram():
     return render_template('diagram-form.html')
+
 @app.route('/prediction-form')
 def predict():
     return render_template('predict-form.html')
+
 @app.route("/diagram", methods=['POST'])
 def diagram_upload():
     dataset_file = request.files.get('dataset')
@@ -38,6 +42,20 @@ def diagram_upload():
             
 @app.route("/predict", methods=['POST'])
 def prediction_upload():
-    return render_template('result-predict.html')
+    dataset_file = request.files.get('dataset')
+    if dataset_file:
+        dataset_file.save(dataset_file.filename)
+
+        data = Predict(str(dataset_file.filename))
+        
+        os.remove(dataset_file.filename)
+
+        matrics, result = data.get_result()
+        
+        return render_template('result-predict.html', matrics=matrics, result=result)
+    else:
+        return "No file uploaded."
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- predict.py
+++ predict.py
@@ -2,37 +2,42 @@ import joblib
 import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-filename = "./test-file/dog.txt"
+class Predict:
 
-def Kmers_funct(seq, size=6):
-    return [seq[x:x+size].lower() for x in range(len(seq) - size + 1)]
+    def __init__(self, filepath):
+        self.filename = filepath
+        data = pd.read_table(self.filename)
+        cv = joblib.load('./model/cv.joblib')
+        classifier = joblib.load('./model/classifier.joblib')
 
-def get_metrics(y_test, y_predicted):
-    accuracy = accuracy_score(y_test, y_predicted)
-    precision = precision_score(y_test, y_predicted, average='weighted')
-    recall = recall_score(y_test, y_predicted, average='weighted')
-    f1 = f1_score(y_test, y_predicted, average='weighted')
-    return accuracy, precision, recall, f1
+        data['words'] = data.apply(lambda x: self.Kmers_funct(x['sequence']), axis=1)
+        data = data.drop('sequence', axis=1)
 
-cv = joblib.load('./model/cv.joblib')
-classifier = joblib.load('./model/classifier.joblib') 
+        texts = list(data['words'])
+        for item in range(len(texts)):
+            texts[item] = ' '.join(texts[item])
 
-data = pd.read_table(filename)
+        self.y_data = data.iloc[:, 0].values
+        self.x_data = cv.transform(texts)
+        self.y_pred = classifier.predict(self.x_data)
 
-data['words'] = data.apply(lambda x: Kmers_funct(x['sequence']), axis=1)
-data = data.drop('sequence', axis=1)
+    def Kmers_funct(self, seq, size=6):
+        return [seq[x:x+size].lower() for x in range(len(seq) - size + 1)]
 
-texts = list(data['words'])
-for item in range(len(texts)):
-    texts[item] = ' '.join(texts[item])
+    def get_metrics(self):
+        accuracy = accuracy_score(self.y_data, self.y_pred)
+        precision = precision_score(self.y_data, self.y_pred, average='weighted')
+        recall = recall_score(self.y_data, self.y_pred, average='weighted')
+        f1 = f1_score(self.y_data, self.y_pred, average='weighted')
+        return accuracy, precision, recall, f1
 
-y_data = data.iloc[:, 0].values
-
-x_data = cv.transform(texts)
-
-y_pred = classifier.predict(x_data)
-
-print("Confusion matrix\n")
-print(pd.crosstab(pd.Series(y_data, name='Actual'), pd.Series(y_pred, name='Predicted')))
-accuracy, precision, recall, f1 = get_metrics(y_data, y_pred)
-print("accuracy = %.3f \nprecision = %.3f \nrecall = %.3f \nf1 = %.3f" % (accuracy, precision, recall, f1))
+    def get_result(self):
+        result = []
+        matrics = pd.crosstab(pd.Series(self.y_data, name='Actual'), pd.Series(self.y_pred, name='Predicted'))
+        accuracy, precision, recall, f1 = self.get_metrics()
+        result.append("accuracy = %.3f" % (accuracy))
+        result.append("precision = %.3f" % (precision))
+        result.append("recall = %.3f" % (recall))
+        result.append("f1 = %.3f" % (f1))
+        
+        return matrics, result
